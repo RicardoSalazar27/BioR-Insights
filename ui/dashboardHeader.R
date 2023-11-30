06f48b8c5796d1e8a09b18df038693d3a51dbd0d
RicardoSalazar <edgar_ss2012@hotmail.com>
2023-11-30 12:03:11 -0600

    se agrego logotipo de la app

--- ui/dashboardHeader.R
+++ ui/dashboardHeader.R
@@ -2,14 +2,7 @@
 
 
 header <- dashboardHeader(
-  
-  
-  title = "BioR Insghts"
-  #title = tags$img(src='GoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg', height = '40', width ='40')
-  
-  #title = tags$img(src='https://drive.google.com/file/d/1rW7bKd92z6ooMHTgCJL-FkoPmwaOvmS6/view', height = '40', width ='40')
-  
-  #title = tags$img(src="logo.png",height = '20', width ='20')
-  #title = tags$img(src='https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg', height = '60', width ='60')
-)
 
+  title = tags$a(href='http://mycompanyishere.com',
+                 tags$img(src='logo333.png',height = 29))
+)
